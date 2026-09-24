58b1576b6c2e3e1dfe3f798846003a66982d18cb
hetalrupareliya91 <hetalrupareliya91@users.noreply.github.com>
2026-09-24 16:03:47 +0530

    feat: add Next.js app shell

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import CoinMazeGame from "../components/CoinMazeGame";
+
+export default function Home() {
+  return (
+    <main className="main">
+      <h1>Coin Maze</h1>
+      <CoinMazeGame />
+    </main>
+  );
+}
